b04142ea2d12bc9443348b9cea864738b16657cd
blocks1623 <blocksincorporated@gmail.com>
2026-04-13 01:21:17 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" data-theme="dark">
+    <html lang="en" data-theme="dark" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
